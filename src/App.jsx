bd5d111edcffcfbe32cfd8bd01d36142d7a5e174
Iuliia Pavlova <iuliia.a.pavlova@gmail.com>
2021-04-15 13:17:33 -0400

    refactor(slider): delete files of the  first attempt on slider

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import './styles/main.scss';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
-// import Slider from './components/Slider';
-// import { projectsData } from './data/data';
 import SliderProjects from './components/SliderProjects';
 
 function App() {
@@ -11,7 +9,6 @@ function App() {
     <div id='shell'>
       <NavBar />
       <Hero />
-      {/* <Slider projectsArr={projectsData} /> */}
       <SliderProjects />
     </div>
   );
